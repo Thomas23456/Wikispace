03b0da69d85b30b709198fcd6148e8b7068f405c
Thomas23456 <thomas.payan@ynov.com>
2020-12-02 16:52:46 +0100

    zizi

---
+++ src/components/Astronaut.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-for="currency in info" :key="currency">
+    <span>
+      {{ currency.name }}
+      <img style="width: 100px; height: 100px; border-radius: 50%;" :src="currency.profile_image" alt="Image astronaute"/>
+      <div id="agence"></div>
+    </span>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Astronaut',
+  data() {
+    return {
+      info : null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://spacelaunchnow.me/api/3.3.0/astronaut/?limit=30')
+      .then(response => (this.info = response.data.results))
+  }
+}
+</script>
